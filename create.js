1281350f05d90390ee9475c0b3b1f37b22d018f5
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2024-12-06 19:42:09 +0000

    update

--- create.js
+++ create.js
@@ -10,53 +10,53 @@ globalThis.config = (await import("./data/create.json", { with: { type: "json" }
 
 globalThis.projectPath = path.join("projects", config.id)
 
-globalThis.project = JSON.parse(fs.readFileSync(projectPath + "/project.json"))
-project.config = structuredClone(config)
-save()
+// globalThis.project = JSON.parse(fs.readFileSync(projectPath + "/project.json"))
+// project.config = structuredClone(config)
+// save()
 
-// if (fs.existsSync(projectPath)) {
-//   throw new Error(`Project "${config.id}" already exists`)
-// }
+if (fs.existsSync(projectPath)) {
+  throw new Error(`Project "${config.id}" already exists`)
+}
 
-// fs.mkdirSync(projectPath)
+fs.mkdirSync(projectPath)
 
-// globalThis.project = {
-//   config: structuredClone(config),
-//   curseforge: {},
-//   planetminecraft: {},
-//   modrinth: {}
-// }
+globalThis.project = {
+  config: structuredClone(config),
+  curseforge: {},
+  planetminecraft: {},
+  modrinth: {}
+}
 
 for (const img of config.images) {
   img.buffer = await sharp(path.join("./data", "images", img.file + ".png")).resize(1920, 1080, { fit: "inside" }).jpeg({ quality: 95 }).toBuffer()
 }
 
-// // CurseForge
+// CurseForge
 
-// await curseforge.createProject()
-// await curseforge.uploadPack()
-// await curseforge.uploadImages()
-// await curseforge.setDetails()
+await curseforge.createProject()
+await curseforge.uploadPack()
+await curseforge.uploadImages()
+await curseforge.setDetails()
 
-// console.log("CurseForge: Project fully created")
+console.log("CurseForge: Project fully created")
 
-// // Planet Minecraft
+// Planet Minecraft
 
-// await planetminecraft.createProject()
+await planetminecraft.createProject()
 
-// console.log("Planet Minecraft: Project fully created")
+console.log("Planet Minecraft: Project fully created")
 
 // Modrinth
 
-// await modrinth.createProject()
-// await modrinth.uploadPack()
+await modrinth.createProject()
+await modrinth.uploadPack()
 await modrinth.uploadImages()
-// await modrinth.setDetails(true)
+await modrinth.setDetails(true)
 
 console.log("Modrinth: Project fully created")
 
-// // Ewan Howell
+// Ewan Howell
 
-// await ewanhowell.createPack()
+await ewanhowell.createPack()
 
-// console.log("Ewan Howell: Project fully created")
+console.log("Ewan Howell: Project fully created")
