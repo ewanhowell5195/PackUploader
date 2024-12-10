0078ef22d483d752603e375550b4798ca0f903fa
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2024-12-10 11:25:58 +0000

    version already exists check

--- update.js
+++ update.js
@@ -14,6 +14,12 @@ globalThis.projectPath = path.join("projects", config.id)
 
 globalThis.project = JSON.parse(fs.readFileSync(projectPath + "/project.json"))
 
+if (project.config.version === config.version) {
+  console.error(`Error: "${config.id}" is already at version ${config.version}`)
+  process.exit()
+}
+
+project.config.version = config.version
 project.config.versions = config.versions
 
 save()
