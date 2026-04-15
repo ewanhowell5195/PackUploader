e55c55398058d2a394e9d851b9e4aea1c70a405e
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2026-04-15 02:01:37 +0100

    unix executables

---
+++ run/details.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")/.."
+node --no-warnings scripts/details.js
+read -n 1 -s -r -p "Press any key to continue..."
--- run/queue.bat
+++ run/queue.bat
@@ -1,3 +1,4 @@
 @echo off
+cd /d "%~dp0.."
 node --no-warnings scripts/queue.js
 pause
